ca1749ebec225510ba09824cf040a0144961e1bb
devkkiri <yoon_1139@naver.com>
2022-08-04 21:12:56 +0900

    naver map api

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useRef } from 'react';
+
 function App() {
-  return (
-    <>
-      <div>Main Branch - Base Settings</div>
-    </>
-  );
+  const mapElement = useRef(null);
+
+  useEffect(() => {
+    const { naver } = window;
+    if (!mapElement.current || !naver) return;
+
+    const location = new naver.maps.LatLng(37.5656, 126.9769);
+    const mapOptions: naver.maps.MapOptions = {
+      center: location,
+      zoom: 17,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: naver.maps.Position.TOP_RIGHT,
+      },
+    };
+    const map = new naver.maps.Map(mapElement.current, mapOptions);
+    new naver.maps.Marker({
+      position: location,
+      map,
+    });
+  }, []);
+
+  return <div ref={mapElement} style={{ minHeight: '400px' }} />;
 }
 
 export default App;
