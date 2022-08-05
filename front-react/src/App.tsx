4693c1344a09315aa1a5a8b53f5620daa58194d2
devkkiri <yoon_1139@naver.com>
2022-08-05 18:37:43 +0900

    slide page

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -1,8 +1,32 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Mousewheel, Keyboard } from 'swiper';
+
+import './styles/App.styles.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 function App() {
   return (
-    <>
-      <div>Main Branch - Base Settings</div>
-    </>
+    <Swiper
+      direction={'vertical'}
+      pagination={{ clickable: true }} //
+      mousewheel
+      keyboard
+      modules={[Pagination, Mousewheel, Keyboard]}
+      allowTouchMove
+      className="main_slider"
+      threshold={20}
+      speed={1000}
+      onActiveIndexChange={(swiper) => {
+        console.log(swiper.activeIndex);
+      }}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+    </Swiper>
   );
 }
 
