ab9d88d9922cc081a5ea21e173059f25724b06d5
devkkiri <yoon_1139@naver.com>
2022-08-11 05:00:27 +0900

    react-intersection-observer: using lib

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -1,5 +1,62 @@
+import axios from 'axios';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 function App() {
-  return <h1>Main Branch</h1>;
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [hasNextPage, setHasNextPage] = useState<boolean>(true);
+  const page = useRef<number>(1);
+  const [ref, inView] = useInView();
+
+  const fetch = useCallback(async () => {
+    try {
+      const { data } = await axios.get<Post[]>(
+        `http://localhost:5000/posts?_limit=10&_page=${page.current}`
+      );
+      setPosts((prevPosts) => [...prevPosts, ...data]);
+      setHasNextPage(data.length === 10);
+      if (data.length) {
+        page.current += 1;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(inView, hasNextPage);
+    if (inView && hasNextPage) {
+      fetch();
+    }
+  }, [fetch, hasNextPage, inView]);
+
+  return (
+    <div>
+      {posts?.map((post) => (
+        <div
+          key={post.id}
+          style={{
+            marginBottom: '1rem',
+            border: '1px solid #000',
+            padding: '8px',
+          }}
+        >
+          <div>userId: {post.userId}</div>
+          <div>id: {post.id}</div>
+          <div>title: {post.title}</div>
+          <div>body: {post.body}</div>
+        </div>
+      ))}
+      <div ref={ref} />
+    </div>
+  );
 }
 
 export default App;
